0e237efdb4554567fee162f257b151a78f5dd709
Sparsh Chaudhari <chat.sparsh@gmail.com>
2026-03-07 23:04:39 +0530

    Delete advanced-features-service directory

--- advanced-features-service/src/main/java/com/fluxbank/advancedfeaturesservice/fixeddeposit/repository/FixedDepositRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fluxbank.advancedfeaturesservice.fixeddeposit.repository;
-
-import com.fluxbank.advancedfeaturesservice.fixeddeposit.entity.FixedDeposit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface FixedDepositRepository extends JpaRepository<FixedDeposit, UUID> {
-}
--- advanced-features-service/src/main/java/com/fluxbank/advancedfeaturesservice/recurringdeposit/repository/RecurringDepositRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fluxbank.advancedfeaturesservice.recurringdeposit.repository;
-
-import com.fluxbank.advancedfeaturesservice.recurringdeposit.entity.RecurringDeposit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RecurringDepositRepository extends JpaRepository<RecurringDeposit, UUID> {
-}
